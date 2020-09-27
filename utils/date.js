e4b55bd40ebd88af319d5c923223b8e94408532b
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2020-09-27 17:36:53 +0300

    Fix the month value in getFormattedDate function.

--- utils/date.js
+++ utils/date.js
@@ -1,7 +1,7 @@
 module.exports = {
 	getFormattedDate(d) {
 		const year = d.getFullYear()
-		const month = (d.getMonth() < 10 ? '0' : '') + d.getMonth()
+		const month = (d.getMonth() < 10 ? '0' : '') + (d.getMonth() + 1)
 		const day = (d.getDate() < 10 ? '0' : '') + d.getDate()
 		const hour = (d.getHours() < 10 ? '0' : '') + d.getHours()
 		const minute = (d.getMinutes() < 10 ? '0' : '') + d.getMinutes()
