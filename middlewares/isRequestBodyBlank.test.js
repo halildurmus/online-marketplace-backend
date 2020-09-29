f7ec15355212a267a0c1f62bdc1b33ee571d2ba3
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2020-09-29 19:52:30 +0300

    Add unit tests for isRequestBodyBlank middleware.

---
+++ middlewares/isRequestBodyBlank.test.js
@@ -0,0 +1,23 @@
+const { APIError } = require('../helpers')
+const { isRequestBodyBlank } = require('./')
+
+it('Should throw an APIError if the req body is empty', async () => {
+	const req = { body: {} }
+	const next = jest.fn()
+	const error = new APIError(400, `Request body can't be blank.`)
+
+	await isRequestBodyBlank(req, {}, next)
+
+	expect(next).toBeCalledWith(error)
+})
+
+it('Should call the next function if the req body is not empty', async () => {
+	const req = { body: { firstName: 'Halil İbrahim', lastName: 'Durmuş' } }
+	const next = jest.fn()
+	const error = new APIError(400, `Request body can't be blank.`)
+
+	await isRequestBodyBlank(req, {}, next)
+
+	expect(next).toBeCalled()
+	expect(next).not.toBeCalledWith(error)
+})
